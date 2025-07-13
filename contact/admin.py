229d389665efda0cb41ab2c84be4ee6b414e5f12
sambovy <sambovy@gmail.com>
2025-07-12 22:53:23 -0300

    criaçao inicial

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact import models
+
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email', 'created_date',
+    search_fields = 'first_name', 'last_name', 'phone', 'email',
+    ordering = 'created_date',
+    #list_filter = 'created_date', 'email',
+    #list_per_page = 10
+    #list_max_show_all = 100
+    list_editable = 'phone', 'email',
+    list_display_links = 'first_name',
+    
