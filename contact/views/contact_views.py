b5627a276b530f0855591afccc46bb449474c290
sambovy <sambovy@gmail.com>
2025-07-13 15:36:36 -0300

    pagina lista completa

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -23,9 +23,12 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk=contact_id, show=True
     )
+    
+    site_title = f'{single_contact.first_name} {single_contact.last_name} -'
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
