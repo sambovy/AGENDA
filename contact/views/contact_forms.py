9399615ce0256f7e4a8189f7e74861c343e69d9d
sambovy <sambovy@gmail.com>
2025-07-13 23:05:07 -0300

    nova atualizacao

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,17 +1,34 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
-
+from django import forms
 from contact.models import Contact
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'email'
+        )
 
 def create(request):
-    context = {
-
+    if request.method == 'POST':
+        context = {
+        'form': ContactForm(request.POST)
     }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm()
+    }
+    
     return render(
         request,
         'contact/create.html',
         context
-    )
+    ) 
